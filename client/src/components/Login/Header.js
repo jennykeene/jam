73549b5ccde74e68ef3f0b74cb445826e8c31fa1
Jenny Keene <jennifer0keene@gmail.com>
2022-04-06 12:49:58 -0500

    Update Header.js

---
+++ client/src/components/Login/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const Header = () => {
+  return (
+    <AppBar
+        component="div"
+        color="primary"
+        position="static"
+        elevation={0}
+        sx={{ zIndex: 0 }}
+    >
+        <Toolbar>
+            <Grid container alignItems="center" spacing={1}>
+                <Grid item xs>
+                <Typography color="inherit" variant="h5" component="h1">
+                    Hello, Peasant.
+                </Typography>
+                </Grid>
+            </Grid>
+        </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header;
