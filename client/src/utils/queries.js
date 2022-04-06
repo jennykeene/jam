ced49f666065ae44adcda2d5f95f84148fbe0ed2
Jenny Keene <jennifer0keene@gmail.com>
2022-04-06 16:06:04 -0500

    Update queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,40 @@
+// this file stores all the GraphQL query requests 
+
+import { gql } from '@apollo/client';
+
+export const QUERY_TASKS = gql`
+    query getTasks {
+        tasks {
+            _id
+            taskText
+        }
+    }
+`;
+
+export const QUERY_USER = gql`
+    query user ($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            tasks {
+                _id
+                taskText
+            }
+        }
+    }
+`
+//query to retrieve all data related to logged-in user 
+export const QUERY_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            tasks {
+                _id
+                taskText
+            }
+        }
+    }
+`
