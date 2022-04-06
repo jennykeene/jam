a0d1e8b6673e9fe239e32911974b952d29c8fce3
Jenny Keene <jennifer0keene@gmail.com>
2022-04-06 16:06:06 -0500

    Update mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,34 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_TASK = gql`
+  mutation addTask($taskText: String!) {
+    addTask(taskText: $taskText) {
+      _id
+      thoughtText
+    }
+  }
+`
