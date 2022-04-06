737880c45cb7df379435b97d458412ad2f056c83
Jenny Keene <jennifer0keene@gmail.com>
2022-04-06 10:08:33 -0500

    Create PleaseSignIn.js

---
+++ client/src/components/Home/PleaseSignIn.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const PleaseSignIn = () => {
+
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+			}}
+		>
+			{/* Options Box */}
+			<Box
+				sx={{
+				mt: 30,
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+			}}
+			>
+				<Typography component="h1" variant="h4" fontWeight='bold' fontFamily='Arial' color='#fff'>
+					Please Login or Signup to continue!
+				</Typography>
+
+				{/***************** Buttons *****************/}
+				<Box noValidate sx={{ mt: 1 }}>
+					<Button
+						href="/login"
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+						Login
+					</Button>
+					<Button
+						href="/signup"
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+						Signup
+					</Button>
+					<Button
+						href="/preview"
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+						Preview the App
+					</Button>
+				</Box>
+			</Box>
+		</Box>
+
+	);
+}
+
+export default PleaseSignIn;
