f31d9c1ec5aca69f7c416d1d777cc653bafb1fa0
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-06 18:16:44 -0500

    Update typeDefs.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+//file for defining every piece of data that client can expect to work with throught a query or mutation
+// think of this file as defining the API endpoint + the exact data and parameters tied to that endpoint
+
+// import gql tagged template fx
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        tasks: [Task]
+    }
+    # instructing tasks query so that each task returns this info
+    type Task {
+        _id: ID
+        taskText: String
+        createdAt: String
+        username: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+        users: [User]
+        user(username: String): User
+        tasks: [Task]!
+        # tasks(username: String!): [Task]
+        task(_id: ID!): Task
+    }
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(username: String!, password: String!): Auth
+        addTask(taskText: String!): Task
+    }
+`;
+
+module.exports = typeDefs;
