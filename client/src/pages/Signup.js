d8e56d7387c4455bb8b9cade7dd0ed25f793f5d9
Jenny Keene <jennifer0keene@gmail.com>
2022-04-06 13:25:17 -0500

    Update Signup.js

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Content from '../components/Signup/Content';
+import Footer from '../components/Signup/Footer';
 import Header from '../components/Signup/Header'
 
 const Signup = () => {
   return (
     <>
       <Header />
+      <Content />
+      <Footer />
     </>
   )
 }
