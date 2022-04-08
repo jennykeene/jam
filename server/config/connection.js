4d96a2367da573754f3985e5c62b0eac3141b31d
Jenny Keene <jennifer0keene@gmail.com>
2022-04-07 19:44:14 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/apollotask',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/jamdb',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
