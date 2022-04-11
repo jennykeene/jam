95aed90fd425a2683d01e4c01dab2c2a0bd7e645
Jenny Keene <jennifer0keene@gmail.com>
2022-04-11 16:38:26 -0500

    Update Kast.js

--- server/models/Kast.js
+++ server/models/Kast.js
@@ -1,21 +1,26 @@
-const { Schema } = require('mongoose');
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
-const kastSchema = new Schema({
-  	kastText: {
-    	type: String,
-    	required: true,
-  	},
-  	createdAt: {
-		type: Date,
-		default: Date.now,
-		get: (createdDateTime) => moment(createdDateTime).format('MM DD YY [at] hh:mm a')
-	},
-	completed: {
-		type: Boolean,
-		default: false
-	},
-});
+const kastSchema = new Schema(
+	{
+		kastText: {
+			type: String,
+			required: true,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdDateTime) => moment(createdDateTime).format('MM DD YY [at] hh:mm a')
+		},
+		completed: {
+			type: Boolean,
+			default: false
+		}
+	}
+);
 
-module.exports = kastSchema;
+//const Kast = model('Kast', kastSchema);
+
+module.exports = kastSchema
 
