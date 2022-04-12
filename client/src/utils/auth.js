718ac0c11e74e8f3ff6e97f8514b55c32250a67a
Jenny Keene <jennifer0keene@gmail.com>
2022-04-12 10:48:29 -0500

    Update auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -37,7 +37,7 @@ class AuthService {
         // Saves user token to localStorage
         localStorage.setItem('id_token', idToken);
 
-        window.location.assign('/dashboard');
+        window.location.assign('/preview');
     }
 
     // clear token from localStorage & force logout with reload
