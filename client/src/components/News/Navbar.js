741069aff2b25029cdd03d4ed9f87b51826a4404
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-12 21:15:51 -0500

    Update Navbar.js

--- client/src/components/News/Navbar.js
+++ client/src/components/News/Navbar.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import { AppBar } from '@mui/material'
 const Navbar = () => {
+
+// navbar backgroundColor, padding, and fontsize
+const navBar = {
+  backgroundColor: "rgb(18, 127, 199)",
+  padding: "22px",
+  fontSize: "20px",
+};
+// navbrand font size, margin left
+const navBrand = {
+  fontSize: "26px",
+  marginLeft: "18px"
+};
+// nav marginleft
+const nav = {
+  marginLeft: "12px"
+};
+
   return (
     <AppBar>
       
