7497a11759cc50b846d6b0efbf715fc187c4696c
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-12 21:10:44 -0500

    Update config.js

---
+++ client/src/utils/API/config.js
@@ -0,0 +1,24 @@
+export const navbarBrand = "JAM News";
+export const header = (category) => `News - Top ${category} Headlines`;
+export const navs = [
+    { nav: "Home", page: "/" },
+    { nav: "General", page: "/general" },
+    { nav: "Business", page: "/business" },
+    { nav: "Sports", page: "/sports" },
+    { nav: "Entertainment", page: "/entertainment" },
+    { nav: "Technology", page: "/technology" }
+]
+
+export const router = [
+    { path: "/", key: "general", category: "general", country: "us" },
+    { path: "/general", key: "general", category: "general", country: "us" },
+    { path: "/business", key: "business", category: "business", country: "us" },
+    { path: "/sports", key: "sports", category: "sports", country: "us" },
+    { path: "/entertainment", key: "entertainment", category: "entertainment", country: "us" },
+    { path: "/technology", key: "technology", category: "technology", country: "us" }
+]
+
+export const summary = "Author, Channel and Date";
+export const author = (author) => `Author: ${!author ? "Unknown" : author}`;
+export const channel = (channel) => `Channel: ${channel}`;
+export const lastUpdate = (date) => `Last updated: ${new Date(date).toGMTString()}`;
