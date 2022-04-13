bd69373b2e0000ec49bfe7cebdad11d5a52b67bb
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-12 21:57:55 -0500

    Update NewsIcons.js

--- client/src/components/News/NewsIcons.js
+++ client/src/components/News/NewsIcons.js
@@ -1,9 +1,65 @@
 import React from 'react'
+import { Button, Card } from "react-bootstrap";
 
-const NewsIcons = () => {
+const NewsIcons = (props) => {
+
+  const card = {
+    borderRadius: "15px",
+    backgroundColor: "rgb(40, 45, 53)",
+    color: "#fff"
+  };
+  // borders for news article image
+  const image = {
+    borderTopLeftRadius: "15px",
+    borderTopRightRadius: "15px",
+  };
+  // export const bttn
+  const bttn = {
+    backgroundColor: "#005abb",
+    borderRadius: "12px",
+    fontWeight: "bold",
+    color: "#fff",
+    boxShadow: "0 30px 36px 0 rgba(0, 0, 0, 0.2)",
+  };
+  //export const detail
+  const detail = {
+    marginBottom: "15px"
+  };
+  // const sum
+  const sum = {
+    color: "#f5f5f5",
+    fontSize: "15px"
+  };
+  
+  // const txt
+  const txt = {
+    color: "#b6b4b4",
+  };
+  // const text
+  const text = {
+    color: "#6c757d",
+    marginBottom: "3px",
+    marginTop: "4px",
+    fontSize: "12px",
+    opacity: 1
+  };
+
+
+  const { imageUrl, alt, description, title, urlNews } = props
   return (
-    <div>NewsIcons</div>
+    <>
+      <Card style={card}>
+        <Card.Img style={image} variant="top" src={imageUrl} alt={alt} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text style={txt}>
+            {description}
+          </Card.Text>
+          <Button href={urlNews} target="_blank" style={bttn}>Read more →</Button>
+        </Card.Body>
+      </Card>
+    </>
   )
 }
 
-export default NewsIcons
+export default NewsIcons;
