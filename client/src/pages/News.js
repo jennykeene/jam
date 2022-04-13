6d7f142bb3e388a2f0b9b9c0ef84f9ffc1e47468
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-12 21:15:53 -0500

    Update News.js

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 // import NavBar from "./components/NavBar/NavBar";
 // import News from "./components/News/News";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-//import { router } from "./config/config";
+import { router } from "../utils/API/config";
 //import LoadingBar from "react-top-loading-bar";
 
 const News = () => {
