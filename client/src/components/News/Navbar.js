eeb4df24c5e753b9208d39f89bedd6786ec901fe
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-12 21:57:57 -0500

    Update Navbar.js

--- client/src/components/News/Navbar.js
+++ client/src/components/News/Navbar.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { v4 as uuidv4 } from "uuid";
 import { navbarBrand, navs } from "../../utils/API/config";
 import { AppBar } from '@mui/material';
-const Navbar = () => {
+import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap"
+const Navigation = () => {
 
 // navbar backgroundColor, padding, and fontsize
 const navBar = {
@@ -23,8 +25,21 @@ const nav = {
   return (
     <AppBar style={navBar} variant="dark" expand="lg" fixed="top">
       Michael Keene, the Dream Queen
+      <Navbar style={navBar} variant="dark" expand="lg" fixed="top">
+      <Navbar.Brand style={navBrand} href="/">{navbarBrand}</Navbar.Brand> 
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav style={nav} className="mk">
+          {navs.map(navs =>
+            <LinkContainer to={navs.page} key={uuidv4()}>
+              <Nav.Link className="mgk">{navs.nav}</Nav.Link>
+            </LinkContainer>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     </AppBar>
   )
 }
 
-export default Navbar
+export default Navigation;
