77fc280679b844300e35fac46e426fbe7258fad6
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-13 23:30:29 -0500

    Delete api.js

--- client/src/utils/API/api.js
+++
@@ -1,5 +0,0 @@
-export const API_DOMAIN = "https://newsapi.org/v2/top-headlines?country="
-export const API_KEY = "fb61515de1e94ba594cf37eaea0e709f"
-
-
-export const endpointPath = (country, category, page, pageSize) => `${API_DOMAIN}${country}&category=${category}&apiKey=${API_KEY}&page=${page}&pageSize=${pageSize}`;
