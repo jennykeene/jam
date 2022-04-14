0edf492fffaa60da4fa45754a15a1b5b420871ea
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-13 23:52:01 -0500

    Update News.js

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -1,18 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import Footer from "../components/News/Footer";
 import Navigation from "../components/News/Navbar";
 import NewsList from "../components/News/NewsList";
-// import { v4 as uuidv4 } from "uuid";
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import { router } from "../utils/API/config";
-//import LoadingBar from "react-top-loading-bar";
+
 
 const News = () => {
-  const [progress, setProgress] = useState(0);
-
-  const pageSize = 7;
-
-  document.body.style.backgroundColor = "rgb(36, 39, 41)";
 
   return (
     <>
