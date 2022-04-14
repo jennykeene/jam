b986a75110ea270a06c8183b0432321262000f84
Alfonso Fernandez <alfonsofernandez4th@gmail.com>
2022-04-14 12:38:05 -0500

    adding comments

--- client/src/pages/Calculator.js
+++ client/src/pages/Calculator.js
@@ -1,9 +1,10 @@
 import React from 'react'
+//imported the javascript for each page into calculator
 import Content from '../components/Calculator/Content';
 import Header from '../components/Calculator/Header';
 import Footer from '../components/Calculator/Footer';
 
-
+// use react to add components to page
 const Calculator = () => {
   return (
     <>
