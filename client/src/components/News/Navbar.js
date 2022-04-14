66f15f8c0c82a85c4b40d33c38f185187cb55eca
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-13 23:46:35 -0500

    Update Navbar.js

--- client/src/components/News/Navbar.js
+++ client/src/components/News/Navbar.js
@@ -1,28 +1,35 @@
-import React from 'react';
+// navbar import
+import React from "react"
 import { v4 as uuidv4 } from "uuid";
-import { navbarBrand, navs } from "../../utils/API/config";
-import { Navbar, Nav } from "react-bootstrap";
+// importing from react-bootstrp in nodemodules
+import { Navbar, Nav } from "react-bootstrap"
+// importing from config.js
+import { navbarBrand, navs } from "../../utils/api"
+// react-routr in nodemodules
 import { LinkContainer } from "react-router-bootstrap"
-const Navigation = () => {
 
-// navbar backgroundColor, padding, and fontsize
-const navBar = {
-  backgroundColor: "rgb(18, 127, 199)",
-  padding: "22px",
-  fontSize: "20px",
-};
-// navbrand font size, margin left
-const navBrand = {
-  fontSize: "26px",
-  marginLeft: "18px"
-};
-// nav marginleft
-const nav = {
-  marginLeft: "12px"
-};
+// navigation bar function~
+function NavBar() {
+  // navbar backgroundColor, padding, and fontsize
+    const navBar = {
+        backgroundColor: "rgb(18, 127, 199)",
+        padding: "22px",
+        fontSize: "20px",
+    };
+    // navbrand font size, margin left
+    const navBrand = {
+        fontSize: "26px",
+        marginLeft: "18px"
+    };
+    // nav marginleft
+    const nav = {
+        marginLeft: "12px"
+    };
 
   return (
-      <Navbar style={navBar} variant="dark" expand="lg" fixed="top">
+    // navbar properties w dark as the variant
+    // navbar brand to show JAM News
+    <Navbar style={navBar} variant="dark" expand="lg" fixed="top">
       <Navbar.Brand style={navBrand} href="/">{navbarBrand}</Navbar.Brand> 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -37,5 +44,4 @@ const nav = {
     </Navbar>
   )
 }
-
-export default Navigation;
+export default NavBar;
