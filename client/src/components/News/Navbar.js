e587dcdd6aba09044d53df2de2e4af0fbdc3b532
Jenny Keene <jennifer0keene@gmail.com>
2022-04-14 11:08:16 -0500

    Update Navbar.js

--- client/src/components/News/Navbar.js
+++ client/src/components/News/Navbar.js
@@ -1,47 +1,99 @@
-// navbar import
-import React from "react"
+import * as React from 'react';
+import Auth from '../../utils/auth';
+import AppBar from '@mui/material/AppBar';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import Toolbar from '@mui/material/Toolbar';
+import { Button } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 import { v4 as uuidv4 } from "uuid";
-// importing from react-bootstrp in nodemodules
-import { Navbar, Nav } from "react-bootstrap"
-// importing from config.js
-import { navbarBrand, navs } from "../../utils/api"
-// react-routr in nodemodules
-import { LinkContainer } from "react-router-bootstrap"
+import { navs } from "../../utils/api"
+const lightColor = 'rgba(255, 255, 255, 0.7)';
+// navigation bar function
 
-// navigation bar function~
-function NavBar() {
-  // navbar backgroundColor, padding, and fontsize
-    const navBar = {
-        backgroundColor: "rgb(18, 127, 199)",
-        padding: "22px",
-        fontSize: "20px",
-    };
-    // navbrand font size, margin left
-    const navBrand = {
-        fontSize: "26px",
-        marginLeft: "18px"
-    };
-    // nav marginleft
-    const nav = {
-        marginLeft: "12px"
-    };
+const Navigation = () => {
 
   return (
     // navbar properties w dark as the variant
     // navbar brand to show JAM News
-    <Navbar style={navBar} variant="dark" expand="lg" fixed="top">
-      <Navbar.Brand style={navBrand} href="/">{navbarBrand}</Navbar.Brand> 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav style={nav} className="mk">
-          {navs.map(navs =>
-            <LinkContainer to={navs.page} key={uuidv4()}>
-              <Nav.Link className="mgk">{navs.nav}</Nav.Link>
-            </LinkContainer>
-          )}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      {/* ******* settings for 3 icons on top right corner ******* */}
+      <AppBar color="primary" position="sticky" elevation={0}>
+        <Toolbar>
+          <Grid container spacing={1} alignItems="center">
+            <Grid sx={{ display: { sm: 'none', xs: 'block' } }} item>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={undefined}
+                edge="start"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs />
+            {/* ******* login link ******* */}
+            <Grid item>
+              <Button
+                href="/"
+                onClick={Auth.logout}
+                variant="body2"
+                sx={{
+                  textDecoration: 'none',
+                  color: lightColor,
+                  '&:hover': {
+                    color: 'common.white',
+                  },
+                }}
+                rel="noopener noreferrer"
+              >
+                home / logout
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+
+
+            <AppBar component="div" color="primary" position="static" elevation={0} sx={{ zIndex: 0 }}>
+                <Toolbar>
+                    <Grid container alignItems="center" spacing={1}>
+                        <Grid item xs>
+                          <Typography color="inherit" variant="h5" component="h1">
+                              JAM News
+                          </Typography>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+
+
+          </AppBar>
+
+          <AppBar position="static" elevation={0} sx={{ zIndex: 0 }}>
+            <Grid container>
+              {navs.map(navs =>
+                <Toolbar sx={{ color: "inherit", }} className="mk" to={navs.page} key={uuidv4()}>
+                  <Link className="mgk" color="inherit" href={navs.nav}>
+                      {navs.nav}
+                  </Link>
+                </Toolbar>
+              )}
+            </Grid>
+          </AppBar>
+        {/* // Bar with tabs (Tasks, News, Weather) */}
+          <AppBar component="div" position="static" elevation={0} sx={{ zIndex: 0 }}>
+            <Tabs value={2} textColor="inherit">
+              <Tab href="/preview" label="Tasks" />
+              <Tab href="/calculator "label="Calculator" />
+              <Tab href="/news"label="News" />
+            </Tabs>
+          </AppBar>
+    
+    </>
   )
 }
-export default NavBar;
+export default Navigation;
