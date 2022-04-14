b9a10969548021a334aea9bf303f53a9b6d4670b
Jenny Keene <jennifer0keene@gmail.com>
2022-04-13 21:44:05 -0500

    Update CreateTask.js

--- client/src/components/Dashboard/CreateTask.js
+++ client/src/components/Dashboard/CreateTask.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -6,42 +6,17 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { useMutation } from '@apollo/client';
-import { ADD_TASK } from '../../utils/mutations';
 
 
 const CreateTask = () => {
-    const [setTaskInputData] = useState({taskText: ""});
-    const [addTask, { error }] = useMutation(ADD_TASK)
 
-    const handleChange = (event) => {
-
-        const taskInputData = document.querySelector('#taskText').value;
-        console.log(taskInputData);
-        setTaskInputData(taskInputData);
-    };
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        const taskText = document.querySelector('#taskText').value;
-
-        try {
-            const { data } = await addTask({ variables: { taskText }});
-            
-            console.log(data)
-            window.location.assign('/dashboard');
-        } catch (err) {
-            console.error(err);
-        }
-    }
 
     return (
         <>
         <Card>
             <CardContent>
                 <Typography>Create Task</Typography>
-                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                <Box component="form" noValidate sx={{ mt: 1 }}>
                     <TextField
                         margin="normal"
                         required
@@ -51,10 +26,9 @@ const CreateTask = () => {
                         type="taskText"
                         id="taskText"
                         autoComplete="taskText"
-                        onChange={handleChange}
+
                     />
                     <Button
-                        type="submit"
                         fullWidth
                         variant="contained"
                         sx={{ mt: 3, mb: 2 }}
@@ -63,7 +37,7 @@ const CreateTask = () => {
                     </Button>
                     <Grid container>
                         <Grid item>
-                            {error && <div> You must login first </div>}
+
                         </Grid>
                     </Grid>
                 </Box>
